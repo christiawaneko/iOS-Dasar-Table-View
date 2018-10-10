ee931d72b369df640f3a1b8aff8482310cf8dd64
Christiawan Eko S <christiawaneko@gmail.com>
2018-10-10 15:39:45 +0700

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Christiawan Eko Saputro on 10/10/18.
+//  Copyright © 2018 Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
